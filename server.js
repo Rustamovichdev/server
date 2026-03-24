33327183ae37e36e89f05c70c3d5d35d2834dfee
Rustamovichdev <rustamovichdev@gmail.com>
2026-03-24 15:55:48 +0500

    adddddd

--- server.js
+++ server.js
@@ -2,16 +2,71 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 // salom
 // salom
-// yangi 
+// yangi
 app.get("/users", (req, res) => {
     console.log("salom");
 });
 
-app.get("/products",(req,res) => {
-    console.log(req.query)
-})
+app.get("/products", (req, res) => {
+    console.log(req.query);
+});
+
+// routing middleware
+app.get("/path", (req, res, next) => {});
+
+// req => clent tomonidan keladigan ma'lumot
+// res => server qaytaradigan javob
+// next() => keyingi function
+
+// req =>
+
+// req.params => dinamik ko'rinishdagi data
+// req.query  =>  so'rovlar (page)
+// req.body
+// req.headers
+// req.method
+// req ....
+
+// res.send => lyuboy data
+// res.json =>
+// res.status =>
+
+app.get("/users/:username/:age", (req, res) => {
+    const { username, age } = req.params;
+    console.log(username, age);
+    // "56"
+});
+
+app.get("/insta", (req, res) => {
+    const { isActive, page, search } = req.query;
+
+    if (isActive === "true") {
+        res.send("success");
+    } else {
+        res.send("no fallow");
+    }
+});
+
+// body
+
+// req.body => clent bizga yuborgan ma'lumot
+
+app.post("/list", (req, res) => {
+    const data = req.body;
+    // age string Number("18")
+    if (+data.age > 40) {
+        res.send("Sizga mumkun!!!!");
+    } else {
+        res.send("Mumkun emasss!!!!");
+    }
+});
+
 app.listen(3000, () => {
     console.log("server ishladi!!!");
 });
+
+// localhost:3000/routing
