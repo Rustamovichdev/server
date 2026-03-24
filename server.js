d1f60efd74740bd9462f1d19de7f1a1b43fab282
Rustamovichdev <rustamovichdev@gmail.com>
2026-03-24 14:26:12 +0500

    add routing

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 
 // salom
+
+app.get("/users", (req, res) => {
+    console.log("salom");
+});
 app.listen(3000, () => {
     console.log("server ishladi!!!");
 });
